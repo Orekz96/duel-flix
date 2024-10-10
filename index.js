abb48b1df38dc2f86fc742a4479827d8115a755a
Richard Orekoya <orekoyadare12@gmail.com>
2024-10-10 22:35:39 +0100

    Use https for api calls

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const autoCompleteConfig = {
     return movie.Title;
   },
   async fetchData(searchTerm) {
-    const response = await axios.get("http://www.omdbapi.com/", {
+    const response = await axios.get("https://www.omdbapi.com/", {
       params: {
         apikey: "a21f1f1d",
         s: searchTerm,
@@ -46,7 +46,7 @@ createAutoComplete({
 let leftMovie;
 let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
-  const response = await axios.get("http://www.omdbapi.com/", {
+  const response = await axios.get("https://www.omdbapi.com/", {
     params: {
       apikey: "a21f1f1d",
       i: movie.imdbID,
